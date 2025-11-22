6119afee6cc286e51aa741970a543737cf7f856e
Do Quang Huy <huydq@admin123.com>
2025-11-22 08:22:59 +0700

    init

---
+++ CsvDataImporter/Editor/CSVImportManger.cs
@@ -0,0 +1,17 @@
+﻿namespace CSvDataImporter.Editor
+{
+    using System.Net;
+    using UnityEngine;
+
+    public static class CSVImportManger
+    {
+        public static string CsvData;
+
+        public static void DownloadCsvData(CsvDataImporterWindow view)
+        {
+            string url = view.SheetUrl;
+            CsvData = new WebClient().DownloadString(url);
+            Debug.Log("CSV Data Downloaded");
+        }
+    }
+}
---
+++ CsvDataImporter/Editor/CsvGenerateWindows.cs
@@ -0,0 +1,89 @@
+namespace  CSvDataImporter.Editor
+{
+    using UnityEditor;
+    using UnityEngine;
+
+    public static class CsvGenerateWindows
+    {
+        [MenuItem("Tools/Csv Data Importer")]
+        public static void OpenTool()
+        {
+            EditorWindow.GetWindow<CsvDataImporterWindow>("CSV Data Importer");
+        }
+    }
+
+    public class CsvDataImporterWindow : EditorWindow
+    {
+        public string SheetUrl     { get; private set; } = "";
+        public string OutputFolder { get; private set; } = "Assets/DataSO";
+        public string SOName       { get; private set; } = "";
+
+        private void OnGUI()
+        {
+            GUILayout.Label("CSV Config", EditorStyles.centeredGreyMiniLabel);
+
+            this.OutputFolderSelect();
+            this.FillTheSheetUrl();
+            this.FillTheSOName();
+            this.ButtonGenerate();
+            this.ButtonRefresh();
+        }
+
+        private void OutputFolderSelect()
+        {
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField("Output Folder:", GUILayout.Width(150));
+            this.OutputFolder = EditorGUILayout.TextField(this.OutputFolder);
+            EditorGUILayout.EndHorizontal();
+        }
+
+        private void FillTheSheetUrl()
+        {
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField("CSV File Path:", GUILayout.Width(150));
+            this.SheetUrl = EditorGUILayout.TextField(this.SheetUrl);
+            EditorGUILayout.EndHorizontal();
+        }
+
+        private void FillTheSOName()
+        {
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField("Scriptable Object Name:", GUILayout.Width(150));
+            this.SOName = EditorGUILayout.TextField(this.SOName);
+            EditorGUILayout.EndHorizontal();
+        }
+
+        private void ButtonGenerate()
+        {
+            GUILayout.BeginHorizontal();
+            GUILayout.FlexibleSpace();
+
+            if (GUILayout.Button("Generate", GUILayout.Height(30), GUILayout.Width(150)))
+            {
+                if(this.SheetUrl == "")
+                {
+                    Debug.LogError("Sheet URL is empty!");
+                    return;
+                }
+                CSVImportManger.DownloadCsvData(this);
+            }
+
+            GUILayout.FlexibleSpace();
+            GUILayout.EndHorizontal();
+        }
+
+        private void ButtonRefresh()
+        {
+            GUILayout.BeginHorizontal();
+            GUILayout.FlexibleSpace();
+
+            if (GUILayout.Button("Refresh",GUILayout.Height(30), GUILayout.Width(150)))
+            {
+                Debug.Log("Refreshing CSV Data");
+            }
+
+            GUILayout.FlexibleSpace();
+            GUILayout.EndHorizontal();
+        }
+    }
+}
